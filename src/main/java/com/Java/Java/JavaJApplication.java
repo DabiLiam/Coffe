4dbdcb2a1f99260cef1b188293fbe6b23f7a6e00
DabiLiam <davikcfranco@gmail.com>
2025-06-13 22:52:25 -0300

    first commit

---
+++ src/main/java/com/Java/Java/JavaJApplication.java
@@ -0,0 +1,13 @@
+package com.Java.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJApplication.class, args);
+	}
+
+}
